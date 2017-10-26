43793c9d9c3e6c4eb10070b7aca799be6047073c
rose_redff <1404248584@qq.com>
2017-10-26 20:06:25 +0800

    taoxin

--- ceshi_web/settings.py
+++ ceshi_web/settings.py
@@ -82,5 +82,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+     os.path.join(BASE_DIR, 'static').replace('\\', '/'),
+)
+
+
+
---
+++ static/index/js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/10/26.
+ */
+
+$(document).ready(function () {
+
+
+	
+
+
+});
