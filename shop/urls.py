89eb88ae753c1dc2f3d018de6681ef8072600888
rose_redff <1404248584@qq.com>
2017-12-07 19:31:56 +0800

    jquery学习的网页2017-12-7

--- shop/urls.py
+++ shop/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns,url
 from .import views
 
 urlpatterns = patterns('',
+	url('^base/$', views.base, name="base"),
 	url('^index/$', views.index, name="index"),
     url('^detai/$', views.detail, name="detail"),
 )
